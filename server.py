c9237f0ae986a75a842a6195088a1ae970d2c697
MuhammadYusriBinYusoff <yusri.yusoff0001@gmail.com>
2024-10-18 09:31:21 -0400

    Initial Commit

---
+++ server.py
@@ -0,0 +1,37 @@
+"""
+This module implements a Flask web application for emotion detection
+based on the input text provided by users.
+"""
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    """
+    Detects emotions from the provided text and returns the response.
+    """
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']} "
+        f"and 'sadness': {response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}."
+    )
+
+@app.route("/")
+def render_index_page():
+    """
+    Renders the index page of the application.
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # This function executes the Flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=4200)
